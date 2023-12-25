662474cdc56b62c6bcbef0413568760d1f049427
Ben Airey <benjaminjohnairey@gmail.com>
2023-12-25 12:26:24 -0600

    linting and minor edit to 'all'

--- panther_seim/metrics.py
+++ panther_seim/metrics.py
@@ -5,12 +5,14 @@ from datetime import datetime
 
 from ._util import GraphInterfaceBase, validate_timestamp
 
+
 class MetricsInterface(GraphInterfaceBase):
+    """An interface for working with metrics in Panther. An instance of this class will be attached
+    to the Panther client object.
+    """
+
     def all(
-        self,
-        start: str | int | datetime,
-        end: str | int | datetime,
-        interval: int = 180
+        self, start: str | int | datetime, end: str | int | datetime, interval: int = 180
     ) -> dict:
         """Retreives all available metrics for the time period.
 
@@ -24,7 +26,7 @@ class MetricsInterface(GraphInterfaceBase):
                 timestamp in UTC. When a string or datetime, if no timezone is specified, we assume
                 UTC is intended.
             interval (int, optional): The interval between metrics checks. Used in breakdowns.
-        
+
         Returns:
             A dictionary with metrics on alerts, queries, and ingestion.
         """
@@ -38,20 +40,14 @@ class MetricsInterface(GraphInterfaceBase):
             raise ValueError("'interval' must be greater than zero.")
 
         # -- Invoke API
-        vargs = {
-            "input": {
-                "fromDate": start,
-                "toDate": end,
-                "intervalInMinutes": interval
-            }
-        }
-        return self.execute_gql("metrics/all.gql", vargs)
-    
+        vargs = {"input": {"fromDate": start, "toDate": end, "intervalInMinutes": interval}}
+        return self.execute_gql("metrics/all.gql", vargs)["metrics"]
+
     def alerts_per_rule(
-            self,
-            start: str | int | datetime,
-            end: str | int | datetime,
-        ) -> dict:
+        self,
+        start: str | int | datetime,
+        end: str | int | datetime,
+    ) -> dict:
         """Retreives a count of alerts, grouped by triggering rule, for the time period.
 
         Args:
@@ -63,7 +59,7 @@ class MetricsInterface(GraphInterfaceBase):
                 When a string, it must be in ISO format. When an integer, it represents a Unix
                 timestamp in UTC. When a string or datetime, if no timezone is specified, we assume
                 UTC is intended.
-        
+
         Returns:
             A dictionary with metrics on alerts, queries, and ingestion.
         """
@@ -72,17 +68,9 @@ class MetricsInterface(GraphInterfaceBase):
         end = validate_timestamp(end)
 
         # -- Invoke API
-        vargs = {
-            "input": {
-                "fromDate": start,
-                "toDate": end
-            }
-        }
+        vargs = {"input": {"fromDate": start, "toDate": end}}
         results = self.execute_gql("metrics/alerts_per_rule.gql", vargs)
-        data = dict()
+        data = {}
         for datum in results["metrics"]["alertsPerRule"]:
-            data[datum["entityId"]] = {
-                "count": datum["value"],
-                "rule_description": datum["label"]
-            }
+            data[datum["entityId"]] = {"count": datum["value"], "rule_description": datum["label"]}
         return data
