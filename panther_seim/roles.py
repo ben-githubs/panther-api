d097762f8d40cf308fc2702746c9932b3148c1f1
Ben Airey <benjaminjohnairey@gmail.com>
2024-02-11 08:22:41 -0800

    added 'get' function for roles

--- panther_seim/roles.py
+++ panther_seim/roles.py
@@ -1,7 +1,7 @@
 """ Code for interacting with Roles.
 """
 
-from ._util import GraphInterfaceBase
+from ._util import GraphInterfaceBase, UUID_REGEX
 
 class RolesInterface(GraphInterfaceBase):
     """An interface for working with roles in Panther. An instance of this class will be attached
@@ -31,3 +31,28 @@ class RolesInterface(GraphInterfaceBase):
         # Invoke GQL
         result = self.execute_gql("roles/list.gql", vargs={"input": vargs})
         return result.get("roles")
+    
+    def get(self, roleid: str) -> dict:
+        """Retreive all details of a single role
+
+        Args:
+            roleid (str): the ID or name of the role you want to fetch.
+                It is preferable to fetch roles by ID, since name is not guaraneed to be a unique
+                indicator for each role.
+
+        Returns:
+            A single role, as a dictionary.
+        """
+
+        # Validate input
+        if not isinstance(roleid, str):
+            raise TypeError(f"Role ID must be a string, not '{type(roleid).__name__}'.")
+
+        # Invoke API
+        if not UUID_REGEX.fullmatch(roleid):
+            # This is an email
+            result = self.execute_gql("roles/get_by_name.gql", {"name": roleid})
+            return result.get("roleByName")
+        # This is an ID
+        result = self.execute_gql("roles/get_by_id.gql", {"id": roleid})
+        return result.get("roleById")
