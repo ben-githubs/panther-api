271bdbf0a111d1dc3c3255b6940a781d72f653f2
ben-githubs <38414634+ben-githubs@users.noreply.github.com>
2024-03-07 20:39:25 -0600

    Fix: Data Models Unit Tests (#17)
    * fixed unit tests, added error handling
    
    * linting and formatting

--- panther_seim/data_models.py
+++ panther_seim/data_models.py
@@ -4,11 +4,10 @@ Reference:
     https://docs.panther.com/panther-developer-workflows/api/rest/data-models
 """
 
-import requests
-
+from typing import Sequence, Mapping
 from ._util import RestInterfaceBase, get_rest_response
 from .exceptions import PantherError, EntityNotFoundError, EntityAlreadyExistsError
-from typing import Sequence, Mapping
+
 
 class DataModelInterface(RestInterfaceBase):
     """An interface for working with data models in Panther. An instance of this class will be
@@ -36,12 +35,11 @@ class DataModelInterface(RestInterfaceBase):
             cursor = results.get("next")
             has_more = cursor
             models += results.get("results", [])
-        
+
         return models
-    
 
     def get(self, model_id: str) -> dict:
-        """ Returns the data model with the provided ID.
+        """Returns the data model with the provided ID.
 
         Args:
             model_id (str): The ID of the data model to fetch.
@@ -54,22 +52,20 @@ class DataModelInterface(RestInterfaceBase):
         return get_rest_response(resp)
 
     @staticmethod
-    def _create(
-            body: str = "",
-            description: str = "",
-            display_name: str = None,
-            enabled: bool = None,
-            log_types: Sequence[str] = None,
-            mappings: Mapping[str, str] = None
+    def _create(  # pylint: disable=too-many-arguments
+        body: str = "",
+        description: str = "",
+        display_name: str = None,
+        enabled: bool = None,
+        log_types: Sequence[str] = None,
+        mappings: Mapping[str, str] = None,
     ) -> dict:
-        """ Creates a data-model dictionary, which can be sent as-is to update a new data model, or
+        """Creates a data-model dictionary, which can be sent as-is to update a new data model, or
         used as a base for creating a data model. Since this dictionary creation is identitcal for
         the 'create' and 'update' functions, we centralize the logic here.
         """
         # -- Craft request body
-        body = {
-            "body": body # Apparently this is required, not sure why
-        }
+        body = {"body": body}  # Apparently this is required, not sure why
         if description:
             body["description"] = description
         if display_name:
@@ -86,28 +82,26 @@ class DataModelInterface(RestInterfaceBase):
             #   or a method name. Nice for them, but we need to convert it to what the API wants.
             body["mappings"] = []
             for k, v in mappings.items():
-                mapping = {
-                    "name": k
-                }
+                mapping = {"name": k}
                 if v.startswith("$."):
                     mapping["path"] = v[2:]
                 else:
                     mapping["method"] = v
                 body["mappings"].append(mapping)
-        
+
         return body
-    
-    def create(
-            self,
-            model_id: str,
-            body: str = "",
-            description: str = "",
-            display_name: str = None,
-            enabled: bool = None,
-            log_types: Sequence[str] = None,
-            mappings: Mapping[str, str] = None
+
+    def create(  # pylint: disable=too-many-arguments
+        self,
+        model_id: str,
+        body: str = "",
+        description: str = "",
+        display_name: str = None,
+        enabled: bool = None,
+        log_types: Sequence[str] = None,
+        mappings: Mapping[str, str] = None,
     ) -> dict:
-        """ Creates a new Data Model.
+        """Creates a new Data Model.
 
         Args:
             id (str): the ID of the data model
@@ -123,7 +117,7 @@ class DataModelInterface(RestInterfaceBase):
                 The dictionary keys are the name of the standard field.
                 The dictionary values are either a Python method name, or a JSON path.
                 JSON paths must start with "$.", while Python methods need not.
-        
+
         Returns:
             The created data model object.
         """
@@ -131,7 +125,7 @@ class DataModelInterface(RestInterfaceBase):
             body, description, display_name, enabled, log_types, mappings
         )
         payload["id"] = model_id
-        
+
         # -- Invoke API
         resp = self._send_request("POST", "data_models", body=payload)
         if resp.status_code == 400:
@@ -140,20 +134,20 @@ class DataModelInterface(RestInterfaceBase):
             msg = f"Data Model ID '{model_id}' is already in use"
             raise EntityAlreadyExistsError(msg)
         return get_rest_response(resp)
-    
-    def update(
-            self,
-            model_id: str,
-            new_id: str = None,
-            body: str = "",
-            description: str = "",
-            display_name: str = None,
-            enabled: bool = None,
-            log_types: Sequence[str] = None,
-            mappings: Mapping[str, str] = None,
-            update_only: bool = False
+
+    def update(  # pylint: disable=too-many-arguments
+        self,
+        model_id: str,
+        new_id: str = None,
+        body: str = "",
+        description: str = "",
+        display_name: str = None,
+        enabled: bool = None,
+        log_types: Sequence[str] = None,
+        mappings: Mapping[str, str] = None,
+        update_only: bool = False,
     ) -> dict:
-        """ Updates an existing Data Model.
+        """Updates an existing Data Model.
 
         Args:
             id (str): the ID of the data model
@@ -174,31 +168,43 @@ class DataModelInterface(RestInterfaceBase):
                 By default, if you try to update a data model that doesn't exist, we simply create
                 a new model according to the parameters passed in. If this behaviour is undesirable
                 then set update_only to False.
-        
+
         Returns:
             The created data model object.
         """
         # -- Check if exists
         if update_only:
-            self.get(model_id) # Will raise EntityNotFound if the item doesn't exist yet
+            self.get(model_id)  # Will raise EntityNotFound if the item doesn't exist yet
 
         # -- Craft Payload
         payload = DataModelInterface._create(
             body, description, display_name, enabled, log_types, mappings
         )
         payload["id"] = new_id or model_id
-        
+
         # -- Invoke API
         resp = self._send_request("PUT", f"data_models/{model_id}", body=payload)
         if resp.status_code == 400:
             raise PantherError(f"Invalid data model: {resp.text}")
         return get_rest_response(resp)
-    
+
     def delete(self, model_id: str) -> None:
-        """ Deletes a data model.
+        """Deletes a data model.
 
         Args:
             id (str): the ID of the model to delete.
         """
 
-        self._send_request("DELETE", f"data_models/{model_id}")
+        resp = self._send_request("DELETE", f"data_models/{model_id}")
+        match resp.status_code:
+            case 204:
+                return
+            case 400:
+                raise PantherError(f"Invalid delete request: {resp.text}")
+            case 404:
+                raise EntityNotFoundError(
+                    f"Cannot delete model with ID {model_id}; ID does not exist"
+                )
+
+        # If none of the status codes above matched, then this is an unknown error.
+        raise PantherError(f"Unknown error with code {resp.status_code}: {resp.text}")
